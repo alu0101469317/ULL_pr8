b6afdef7a6b6ae34a0516b311f3858671ed58072
Marco Napierski <alu0101469317@ull.edu.es>
2024-03-12 23:31:50 +0000

    plantilla3

--- tests/basicFunctions.spec.ts
+++ tests/basicFunctions.spec.ts
@@ -1,7 +1,9 @@
 import 'mocha';
 import {expect} from 'chai';
 import {add} from "../src/basicFunctions.js";
-
+/**
+ * Test for basicFunctions.js
+ */
 describe("add function tests", () => {
   it("add(1, 8) returns value 9", () => {
     expect(add(1, 8)).to.be.equal(9);
