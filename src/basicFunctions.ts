73eac4f4bdd4b9cd3ec9ef5a3729e15d6fa17f44
Marco Napierski <alu0101469317@ull.edu.es>
2024-03-13 10:38:51 +0000

    terminado

--- src/basicFunctions.ts
+++ src/basicFunctions.ts
@@ -1,12 +1,82 @@
+
+export interface Event<T> {
+  id: string
+  data: T
+}
+
+interface Observable<T> {
+  subscribe(observer: Observer<T>): void;
+  unsubscribe(observer: Observer<T>): void;
+  notify(event: Event<T>): void;
+}
+
 /**
- * Adds two __numbers__
- * @param firstNumber Consists of the first operand of the addition
- * @param secondNumber Consists of the second operand of the addition
- * @returns The addition of the two numbers `firstNumber` and `secondNumber`
- * ```typescript
- * add(1, 7) = 8
- * ```
+ * Interface for observer classes
  */
-export function add(firstNumber: number, secondNumber: number) {
-  return firstNumber + secondNumber;
+interface Observer<T> {
+  update(observable: Observable<T>, event: Event<T>): void;
+}
+
+export enum NotificacionClickEventType {'NO_EVENT', 'SI_EVENT'};
+
+/**
+ * Class Notificacion that implements the Observable interface, i.e.,
+ * Notificacion objects can be observed
+ */
+export class Notificacion<T> implements Observable<T> {
+  private observers: Observer<T>[] = [];
+
+  private eventType: NotificacionClickEventType = NotificacionClickEventType.NO_EVENT;
+
+  constructor(public id: number, public data: T) {
+  }
+  getEventType() {
+    return this.eventType;
+  }
+
+  subscribe(observer: Observer<T>) {
+    if (this.observers.includes(observer)) {
+      throw new Error('The observer had already been subscribed');
+    } else {
+      this.observers.push(observer);
+    }
+  }
+
+  unsubscribe(observer: Observer<T>) {
+    const index = this.observers.indexOf(observer);
+    if (index === -1) {
+      throw new Error('The observer has not been subscribed');
+    } else {
+      this.observers.splice(index, 1);
+    }
+  }
+  onEvent(event: Event<T>) {
+    this.eventType = NotificacionClickEventType.SI_EVENT
+    this.notify(event);
+  }
+  notify(event: Event<T>) {
+    this.observers.forEach((observer) => observer.update(this,event));
+  }
+}
+
+/**
+ * Class NotificacionObserver that implements the interface Observer, i.e.,
+ * it is able to observe other objects
+ */
+export class NotificacionObserver<T> implements Observer<T> {
+  constructor(private id: number, private name: string) {
+  }
+  update(observable: Observable<T>, evento : Event<T>) {
+    console.log("UPDATED");
+    if (observable instanceof Notificacion) {
+      console.log("IS NOTIFICATION")
+      switch(observable.getEventType()) {
+        case NotificacionClickEventType.SI_EVENT:
+          console.log("ID: " + evento.id);
+          console.log("DATA: " + evento.data);
+          break;
+      }
+    }
+
+  }
 }
--- tests/basicFunctions.spec.ts
+++ tests/basicFunctions.spec.ts
@@ -1,12 +1,48 @@
 import 'mocha';
 import {expect} from 'chai';
-import {add} from "../src/basicFunctions.js";
+import {Notificacion , NotificacionObserver, Event, NotificacionClickEventType} from '../src/basicFunctions.js'
 /**
  * Test for basicFunctions.js
  */
 describe("add function tests", () => {
-  it("add(1, 8) returns value 9", () => {
-    expect(add(1, 8)).to.be.equal(9);
-  });
+  
+// Client code
+const myNotificacion = new Notificacion<string>(0, 'myNotificacion');
+const firstNotificacionObserver = new NotificacionObserver(0, 'firstNotificacionObserver');
+const secondNotificacionObserver = new NotificacionObserver(1, 'secondNotificacionObserver');
+const evento : Event<string>=  {
+  id: "iashd2932h8",
+  data: "hola"
+}
+it("comprabar instacia notificacion 0", () => {
+  expect(myNotificacion).to.be.instanceOf(Notificacion<string>)
+})
+it("comprabar instacia notificacion first", () => {
+  expect(firstNotificacionObserver).to.be.instanceOf(NotificacionObserver)
+})
+it("comprabar instacia notificacion second", () => {
+  expect(secondNotificacionObserver).to.be.instanceOf(NotificacionObserver)
+})
 
+  console.log('firstNotificacionObserver subscription');
+  myNotificacion.subscribe(firstNotificacionObserver);
+
+  console.log('secondNotificacionObserver subscription');
+  myNotificacion.subscribe(secondNotificacionObserver);
+
+  try {
+    myNotificacion.subscribe(secondNotificacionObserver);
+  } catch (error) {
+    console.log('secondNotificacionObserver was already subscribed');
+  }
+
+  it("comprabar evento", () => {
+    expect(myNotificacion.getEventType() == NotificacionClickEventType.NO_EVENT).to.deep.equal(false);
+  })
+  console.log('myNotificacion on event');
+  myNotificacion.onEvent(evento);
+
+  it("comprabar evento", () => {
+    expect(myNotificacion.getEventType() == NotificacionClickEventType.SI_EVENT).to.deep.equal(true);
+  })
 });
